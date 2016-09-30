85a309c1dd71c61b53aab8795c43e35033c7b52c
David Parker <davparker@users.noreply.github.com>
2016-09-29 22:34:24 -0500

    Module 3 work

---
+++ Mod-3_Packages.py
@@ -0,0 +1,14 @@
+# Module 3 Packages
+import numpy
+print(numpy.array([1, 2, 3, 4]))
+
+import numpy as np
+np1 = np.array([1, 2, 3, 4, 5, 6])
+print(type(np1))
+print(np1)
+
+from numpy import array
+np2 = array([7, 8, 9, 0])
+print(type(np2))
+print(np2)
+
